7f852c981a243f56845cf4f5d3c1b887201ea601
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2020-01-07 13:41:01 +0000

    Final CA upload

--- javascript/main.js
+++ javascript/main.js
@@ -12,65 +12,20 @@
 // Global Variables
 //---------------------------
 let molecules, moleculeKey = [];
-let collisonNo,checks = 0;
+let collisonNo,checknum = 0;
 let minRadius = 10, maxRadius = 20, minVelocity = -3, maxVelocity = 3, colWidth, rowHeight;
 //Create an object to hold Variables for gui. - GUI cannot get handle on scope otherwise, unless var is used (Undesirable as it is outdated)
 let guiVars = {
-    numOfMolecules: 20,
+    numOfMolecules: 50,
     numRows: 4,
-    numCols: 3,
-    radiusBaseline:50,
+    numCols: 8,
+    radiusBaseline:5,
     showGrid:true,
     render:true,
     showTrails:false
 };
- 
-//---------------------------
-//P5 Functions
-//---------------------------
-/**
- * Canvas Set up
- */
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(127);
-    //noLoop();
 
-    //Create Gui
-    addGui();
-    //Generate the Initial Moleules
-    generateMolecules(); 
-}
 
-/**
- * P5 function
- * Draw each frame
- */
-function draw() {
-    //Regenerate cell calculations
-    colWidth = width / guiVars.numCols;
-    rowHeight = height / guiVars.numRows;
-    
-    //Show trail behind balls
-    guiVars.showTrails?background(0,0,0,125):background(0);
-    //Draw the grid in the background
-    if(guiVars.showGrid) drawGrid();
-
-    //Run checks
-    splitIntoGridsBroad();
-    checkIntersections();
-    //algorithimRunner(1,molecules);
-
-    //Render molecules
-    if(guiVars.render)renderGrid();
-
-    /**
-     * FrameRate display
-     */
-    textSize(32);
-    fill("#FFF");
-    text(int(getFrameRate()), 30, 30);
-}
 
 //---------------------------
 //Functions
@@ -82,7 +37,7 @@ function draw() {
 function addGui(){
     let gui = new dat.GUI();
     gui.domElement.id = 'gui';
-    gui.add(guiVars, 'numOfMolecules', 0, 10000).onChange(() => generateMolecules()).step(1);//-Regenerates the molecules when changed
+    gui.add(guiVars, 'numOfMolecules', 0, 1000).onChange(() => generateMolecules()).step(1);//-Regenerates the molecules when changed
     gui.add(guiVars, 'radiusBaseline', 0, 100).step(1);
     gui.add(guiVars, 'numRows', 1, 30).step(1);
     gui.add(guiVars, 'numCols', 1, 30).step(1);
@@ -129,7 +84,7 @@ function renderGrid() {
 
 
 /**
- * Broadphase - put the molecules into all of the cells that they are in or overlapping
+ * Broadphase split - put the molecules into all of the cells that they are in or overlapping
  */
 function splitIntoGridsBroad() {
     //Initialise the Array
@@ -181,11 +136,14 @@ function splitIntoGridsBroad() {
             moleculeKey[closestXCell].push(molecule.arrayPosition);
         }
         if (closestYCell >= 0 && closestYCell < moleculeKey.length){
-            moleculeKey[closestYCell].push(molecule.arrayPosition);}
+            moleculeKey[closestYCell].push(molecule.arrayPosition);
+        }
         })
 }
 
-//Narrowphase split into grids
+/**
+ * Narrowphase split -  put the molecules into the cell they are currently in
+ */
 function splitIntoGridsNarrow() {
     //Empty Array
     moleculeKey = [];
@@ -203,17 +161,18 @@ function splitIntoGridsNarrow() {
     })
 }
 
+/**
+ * Check cell for collisions if there are more than 1 in the cell
+ */
 function checkIntersections() {
     moleculeKey.forEach(key => {
-        /**
-         * Check cell for collisions if there are more than 1 in the cell
-         */
         if (key.length > 1) {
             for (i = 0; i < key.length; i++) {
                 for (j = i + 1; j < key.length; j++) {
                     if (p5.Vector.sub(molecules[key[i]].position, molecules[key[j]].position).mag() < molecules[key[i]].radius + molecules[key[j]].radius) {
                         molecules[key[i]].isFilled = true;
                         molecules[key[j]].isFilled = true;
+                        collisonNo++;
                     }
                 }
             }
@@ -221,28 +180,33 @@ function checkIntersections() {
     })
 }
 
+function bruteforceCheck(){
+    for (i = 0; i < molecules.length; i++) {
+        for (j = i + 1; j < molecules.length; j++) {
+            if (p5.Vector.sub(molecules[i].position, molecules[j].position).mag() < molecules[i].radius * 2) {
+                molecules[i].isFilled = true;
+                molecules[j].isFilled = true;
+                collisonNo++;
+            }
+        }
+    }
+}
 
 //---------------------------
 //Runner helper function
 //---------------------------
-function algorithimRunner(algorithimNum, moleculeArray) {
-    let checkTime, checkNum = 0;
+function algorithimRunner(algorithimNum) {
+    checkNum = 0;
     collisonNo = 0;
-    console.time(checkTime)
+    console.time("Total Time")
+    
     switch (algorithimNum) {
         /**
          * Algorithim 1 - BruteForce
          * Simplest Algorithm to code at the expensive of being very resource intensive
          */
         case 0:
-            for (i = 0; i < moleculeArray.length; i++) {
-                for (j = i + 1; j < moleculeArray.length; j++) {
-                    checkNum++;
-                    if (p5.Vector.sub(moleculeArray[i].position, moleculeArray[j].position).mag() < moleculeArray[i].radius * 2) {
-                        collisonNo++;
-                    }
-                }
-            }
+            bruteforceCheck()
         break
         /**
          * Algorithim 2 - Narrowphase
@@ -251,19 +215,25 @@ function algorithimRunner(algorithimNum, moleculeArray) {
          * +Is very fast at estimating collisions
          */
         case 1:
+            console.time("Split Time")
+            splitIntoGridsNarrow();
+            checkIntersections();
+            console.timeEnd("Split Time")
         break
         /**
-         * Algorithim 2 - Narrowphase
-         * Checks within own cell for collisions
-         * -Is innaccurate unless grid is smaller than min radius
-         * +Is very fast at estimating collisions
+         * Algorithim 2 - BroadPhase
+         * Checks Cells around it aswell
          */
         case 2:
-        
+            console.time("Split Time")
+            splitIntoGridsBroad();
+            checkIntersections();
+            console.timeEnd("Split Time")
         break
 
     }
 
-    console.timeEnd(checkTime)
-    console.log('Number of checks' + checkNum)
+    console.timeEnd("Total Time")
+    //console.log('Number of checks' + checkNum)
+    console.log('Number of collisionNo ' + collisonNo)
 }
